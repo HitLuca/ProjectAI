8e0e5131e2493c312b575ae20ffd09f9ba83f77c
HitLuca <luca.simonetto.94@gmail.com>
2017-06-07 21:18:36 +0200

    multiple minor updates

---
+++ Assets/Scripts/ActionController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ActionController : MonoBehaviour {
+    public Camera camera;
+
+    WorldController WorldControllerScript;
+
+    void Start()
+    {
+        WorldControllerScript = GameObject.Find("WorldController").GetComponent<WorldController>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(new Vector2(Screen.width /2, Screen.height/2));
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+                if (isCubeSide(objectHit))
+                {
+                    WorldControllerScript.DestroyObject(objectHit.parent.gameObject);
+                }
+                else
+                {
+                    Debug.Log("Not deletable!");
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+                if (isCubeSide(objectHit))
+                {
+                    Vector3 newCoordinates = CalculateNewCoordinates(objectHit.parent.transform.position, objectHit.gameObject.name);
+                    WorldControllerScript.PlaceObject(objectHit.parent.gameObject, newCoordinates);
+                }
+            }
+        }
+
+    }
+
+    bool isCubeSide(Transform o)
+    {
+        string objectName = o.gameObject.name;
+
+        string[] sides = { "Front", "Back", "Top", "Bottom", "Left", "Right" };
+        if (sides.Contains(objectName))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    Vector3 CalculateNewCoordinates(Vector3 currentPosition, string name)
+    {
+        Vector3 delta = Vector3.zero;
+        switch(name)
+        {
+            case "Front":
+                {
+                    delta = new Vector3(0, 0, 1);
+                    break;
+                }
+            case "Back":
+                {
+                    delta = new Vector3(0, 0, -1);
+                    break;
+                }
+            case "Top":
+                {
+                    delta = new Vector3(0, 1, 0);
+                    break;
+                }
+            case "Bottom":
+                {
+                    delta = new Vector3(0, -1, 0);
+                    break;
+                }
+            case "Left":
+                {
+                    delta = new Vector3(-1, 0, 0);
+                    break;
+                }
+            case "Right":
+                {
+                    delta = new Vector3(1, 0, 0);
+                    break;
+                }
+
+        }
+        return currentPosition + delta;
+    }
+}
---
+++ Assets/Scripts/WorldController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void PlaceObject(GameObject targetObject, Vector3 coords)
+    {
+        GameObject clone = (GameObject)Instantiate(targetObject, coords, Quaternion.identity);
+        clone.transform.position = coords;
+    }
+
+    public void DestroyObject(GameObject targetObject) {
+        Destroy(targetObject);
+    }
+}
