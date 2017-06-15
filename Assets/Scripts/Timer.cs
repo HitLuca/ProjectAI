7c41ca4458dab2c362b14f9a985568e1fe119e35
NSavov <nedko066@gmail.com>
2017-06-15 12:45:07 +0200

    added inputsimulator

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer{
+    float timeLeft = -1;
+    bool timerTrigger = false;
+    delegate void TimerCallback();
+    TimerCallback timerCallback;
+
+    public interface OnTimerDoneListener
+    {
+        void OnTimerDone();
+    }
+
+    OnTimerDoneListener onTimerDoneListener;
+
+    public void SetOnTimerDoneListener(OnTimerDoneListener onTimerDoneListener)
+    {
+        this.onTimerDoneListener = onTimerDoneListener;
+    }
+
+    private void StartTimer(float time)
+    {
+        timeLeft = time;
+        timerTrigger = true;
+    }
+
+    private void StopTimer()
+    {
+        timerTrigger = false;
+    }
+
+    private void UpdateTimer()
+    {
+        if (timerTrigger)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                timerTrigger = false;
+                TimerDone();
+            }
+        }
+    }
+
+    private void TimerDone()
+    {
+
+
+        if (onTimerDoneListener != null)
+            onTimerDoneListener.OnTimerDone();
+
+        //Debug.Log("Done");
+        //actionFinished = true;
+        //actionSequence.advance();
+    }
+
+    
+}
