a18af3fb8c57f7267ab712651fb7298e3ac67770
NSavov <nedko066@gmail.com>
2017-06-16 17:03:14 +0200

    timezones fix

--- Assets/Scripts/CaptionController.cs
+++ Assets/Scripts/CaptionController.cs
@@ -847,7 +847,7 @@ public class CaptionController : MonoBehaviour
     }
 
     private static readonly DateTime UnixEpoch =
-    new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Local);
 
     public static long GetCurrentUnixTimestampMillis()
     {
